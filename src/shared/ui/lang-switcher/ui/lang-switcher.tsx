bf547841f26fa364eb6860d52d68a75a9e88924d
Igidali.Burzhaliev <Igidali.Burzhaliev@ic-group.ru>
2024-11-27 22:47:54 +0300

    add jest

--- src/shared/ui/lang-switcher/ui/lang-switcher.tsx
+++ src/shared/ui/lang-switcher/ui/lang-switcher.tsx
@@ -1,7 +1,6 @@
 import { useTranslation } from "react-i18next";
 import { Button, ThemeButton } from "shared/ui/button";
 import { classNames } from "shared/lib/classnames/classnames";
-import cls from "./lang-switcher.module.scss";
 
 interface LangSwitcherProps {
   className?: string;
@@ -16,7 +15,7 @@ export const LangSwitcher = ({ className }: LangSwitcherProps) => {
 
   return (
     <Button
-      className={classNames(cls.LangSwitcher, {}, [className])}
+      className={classNames("", {}, [className])}
       theme={ThemeButton.CLEAR}
       onClick={toggle}
     >
--- src/shared/ui/theme-switcher/ui/theme-switcher.tsx
+++ src/shared/ui/theme-switcher/ui/theme-switcher.tsx
@@ -1,7 +1,6 @@
 import { Theme, useTheme } from "app/providers/theme-provider";
 import { FC } from "react";
 import { classNames } from "shared/lib/classnames/classnames";
-import cls from "./theme-switcher.module.scss";
 import LightIcon from "shared/assets/icons/theme-light.svg";
 import DarkIcon from "shared/assets/icons/theme-dark.svg";
 import { Button } from "shared/ui/button";
@@ -18,7 +17,7 @@ export const ThemeSwitcher: FC<ThemeSwitcherProps> = ({ className }) => {
     <Button
       theme={ThemeButton.CLEAR}
       onClick={toggleTheme}
-      className={classNames(cls.ThemeSwitcher, {}, [className])}
+      className={classNames("", {}, [className])}
     >
       {theme === Theme.LIGHT ? <LightIcon /> : <DarkIcon />}
     </Button>
